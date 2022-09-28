f2b631b89b7eb6a6c72de5c5da1d123fc46ca962
lee951109 <sk985326@naver.com>
2022-09-28 22:19:27 +0900

    routes setting

---
+++ packages/server/src/routes/index.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import chat from "./chat";
+import room from "./room";
+import user from "./user";
+
+const router = Router();
+
+router.use("/chat", chat);
+router.use("/user", user);
+router.use("/room", room);
+
+export default router;
--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,6 +1,16 @@
 import express, { Application } from "express";
+import router from "./routes";
+import sequelize from "./sequelize";
 
 const app: Application = express();
+
+// request body 사용 가능 & routes 설정
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+sequelize.sync({ force: true });
+
+app.use("/", router);
 
 app.listen(8000, () => {
   console.log("start");
