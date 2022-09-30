0f18ef39c92a3c268c2fb90e234dbd4815e05f59
lee951109 <sk985326@naver.com>
2022-09-30 12:02:32 +0900

    session setting

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,9 +1,21 @@
 import express, { Application } from "express";
+import session from "express-session"; //express 에서 session을 사용할 수 있도록 해주는 패키지
 import router from "./routes";
 import sequelize from "./sequelize";
 
+const FileStore = require("session-file-store")(session);
+
 const app: Application = express();
 
+const sessionMiddleware = session({
+  secret: "kakaoibs", // cookie를 의미로 변조하는 것을 방지하기 위함. => session을 암호화 하여 저장
+  saveUninitialized: true, // session을 저장되기 전에 Uninitialize 상태로 미리 만들어서 저장하겠다
+  cookie: { secure: false },
+  resave: false,
+  store: new FileStore(),
+});
+
+app.use(sessionMiddleware);
 // request body 사용 가능 & routes 설정
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
