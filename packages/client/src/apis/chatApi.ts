79bc481018e855f150a536787a0591f7c24fe2fb
lee951109 <sk985326@naver.com>
2022-10-04 21:51:59 +0900

    API setting

---
+++ packages/client/src/apis/chatApi.ts
@@ -0,0 +1,10 @@
+import axiosInstance from "./axiosInstance";
+
+
+export function fetchChatMessageList(roomId: string) {
+    return axiosInstance.get(`/chat/${roomId}`);
+}
+
+export function sendChatMessage(roomId: string, content: string) {
+    return axiosInstance.post(`/chat/${roomId}`, {content});
+}
---
+++ packages/client/src/apis/userApi.ts
@@ -0,0 +1,22 @@
+import axiosInstance from "./axiosInstance";
+
+
+interface User {
+    username: string;
+};
+
+export function fetchUserList(){
+    return axiosInstance.get("/user");
+}
+
+export function fetchMyProfile() {
+    return axiosInstance.get("/user/me");
+}
+
+export function login(body:User) {
+    return axiosInstance.post("/user/login", body);
+}
+
+export function logout() {
+    return axiosInstance.post("/user/logout");
+}
